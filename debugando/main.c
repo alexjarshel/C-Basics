976cee05dfae23218920739587bfae7f6d16ccbb
alexjarshel <alexbaukartjarschel@gmail.com>
2024-05-27 20:21:41 -0300

    .

---
+++ debugando/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Mensagem 1 \n");
+    printf("Mensagem 2 \n");
+    printf("Mensagem 3 \n");
+    printf("Mensagem 4 \n");
+
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("fala fio bao \n");
+    return 0;
+}
