1938af715ff7d2efe3fb70ac4a00b6df4a8bef52
alexjarshel <alexbaukartjarschel@gmail.com>
2024-06-03 20:45:38 -0300

    .

---
+++ input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    int idade = 0;
+    int ano = 1950;
+    printf ("Valor inicial da Idade %d. \n", idade);
+
+    printf ("Digite o valor da Idade e do ano \n");
+
+    scanf("%d %d" , &idade , &ano);
+
+    printf ("Valor Final da Idade %d.\n", idade);
+    printf ("Valor Final do ano %d.\n", ano);
+
+
+
+
+}
